dfcb15ca851355476d268ae71d5006d8506e653b
Banneq <mateuszsen99@gmail.com>
2021-05-22 03:03:54 +0200

    Updates

--- src/constants/sortType.js
+++
@@ -1,4 +0,0 @@
-export const NONE = "NONE";
-export const ASC = "ASC";
-export const DESC = "DESC";
-export const SORT_TYPES = [NONE, ASC, DESC];
--- src/constants/titles.js
+++
@@ -1,4 +0,0 @@
-export const CAPSULES = "Capsules";
-export const CREW = "Crew";
-export const ROCKETS = "Rockets";
-export const STARLINK = "Starlink";
